63aec0fba33fedd6354f5e99b6080fe7be53e457
Your Name <gitongaeric01@gmail.com>
2022-06-23 10:34:27 +0300

    javascript assignment submission

---
+++ javaIntroduction.js
@@ -0,0 +1,15 @@
+
+// function detail(){
+//     let name='Eric Gituma.';
+//     let height='170cm.';
+//     let country='Kenya.';
+
+//     document.getElementById('name').innerHTML = "My name is " + name ;
+//     document.getElementById('height').innerHTML = "My height is " + height ;
+//     document.getElementById('country').innerHTML = "My county is " + country ;     
+// }
+
+let name='Eric Gituma.';
+let height='170cm.';
+let country='Kenya.';
+console.log("My name is " + name +  "My height is "+ height + "My country is " + country );
